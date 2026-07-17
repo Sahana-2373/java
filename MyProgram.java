2770a1ca08c6ec8f965168f1669ba6e4a78c259a
Sahana-2373 <sahanaselva2373@gmail.com>
2026-07-17 14:58:34 +0530

    ex-2

---
+++ MyProgram.java
@@ -0,0 +1,17 @@
+class MyProgram {
+public static void main(String[] args){
+	byte a=126;
+	short b=32767;
+	int c=2147483647;
+	long d=9223372036854775806L;
+	float e=1.23f;
+	double f= 1.234567;
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+System.out.println(d);
+System.out.println(e);
+System.out.println(f);
+System.out.println("jeeva sir is Best Teacher");
+}
+}
