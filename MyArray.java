f3a38d7c9487eee06426db35cbf031901ea8b24f
Sahana-2373 <sahanaselva2373@gmail.com>
2026-07-17 23:49:18 +0530

    ex3-a

---
+++ MyArray.java
@@ -0,0 +1,23 @@
+class MyName {
+public static void main(String[] args){
+	int myarr[];
+	myarr= new int[]{99,77,66,33,21};
+	int myarr1[]= new int[5];
+	myarr1[0]=11;
+	myarr1[1]=22;
+	myarr1[2]=33;
+	myarr1[3]=41;
+	myarr1[4]=55;
+System.out.println("a[0]" +myarr[0]);
+System.out.println("a[1]" +myarr[1]);
+System.out.println("a[2]" +myarr[2]);
+System.out.println("a[3]" +myarr[3]);
+System.out.println("a[4]" +myarr[4]);
+
+System.out.println("b[0]=" +myarr1[0]);
+System.out.println("b[1]=" +myarr1[1]);
+System.out.println("b[2]=" +myarr1[2]);
+System.out.println("b[3]=" +myarr1[3]);
+System.out.println("b[4]=" +myarr1[4]);
+}
+}
