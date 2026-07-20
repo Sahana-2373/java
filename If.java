44012e5e8b2ff67a29b0c411e2b6da44ad9ce439
Sahana-2373 <sahanaselva2373@gmail.com>
2026-07-20 23:23:25 +0530

    ex4

---
+++ If.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class If{
+	public static void main(String[] args){
+	Scanner ob = new Scanner(System.in);
+	int x;
+	int y;
+	x=ob.nextInt();
+	y=ob.nextInt();
+	if(x>y){
+	System.out.println("x is greater");
+}else {
+	System.out.println("y is greater");
+}
+}
+	
+}
