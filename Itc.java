c1449c33b87600b54664f6d1a36c4190402e96fd
Sahana-2373 <sahanaselva2373@gmail.com>
2026-07-30 22:32:29 +0530

    ex8

---
+++ Itc.java
@@ -0,0 +1,14 @@
+class Itc{
+	int a=5;
+	public static void main(String[] args){
+	Itc student1 = new Itc();
+	Itc student2 = new Itc();
+	Itc student3 = new Itc();
+	Itc student4 = new Itc();
+    System.out.println(student1.a);
+    System.out.println(student2.a);
+    System.out.println(student3.a);
+    System.out.println(student4.a);
+    
+	}
+}
